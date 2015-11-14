45b957779bcc645f3951704ad04cddd4ececb194
YamakasY <yamaksy@gmail.com>
2015-11-14 12:09:16 +0100

    Chapter 5

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -8,11 +8,19 @@ import static org.junit.Assert.assertTrue;
  * Created by YDNPC on 14/11/2015.
  */
 public class MoneyTest {
+
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
+    }
+    
+    @Test
+    public void tesFranctMultiplication(){
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 
     @Test
